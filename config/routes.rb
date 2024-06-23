29c4c37a05525be7d273a5192920897205c97149
Henry Godwin <henryman53@yahoo.co.uk>
2024-06-23 02:02:29 -0230

    Commiting the article listing page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'home', to: 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show]
+  resources :articles#, only: [:show, :index]
 end
