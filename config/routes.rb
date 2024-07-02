611c96149f58404b81249685092866dc6627fbfd
Henry Godwin <henryman53@yahoo.co.uk>
2024-07-02 00:44:35 -0230

    Commiting the article edit page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'home', to: 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles#, only: [:show, :index]
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
 end
